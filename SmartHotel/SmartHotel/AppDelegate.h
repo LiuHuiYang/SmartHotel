9425175796cb0b1f127de9e8a3db0d1d7f40c245
Mac <liuhuiyang2009@163.co>
2018-04-08 11:34:07 +0800

    创建项目

---
+++ SmartHotel/SmartHotel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHotel
+//
+//  Created by Mac on 2018/4/8.
+//  Copyright © 2018年 SmartHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
