b92267aa2a7821471363fd34a8458eacad9d89e2
Mac <liuhuiyang2009@163.co>
2018-04-08 14:37:46 +0800

    首页语言的切换实现

--- SmartHotel/SmartHotel/Classes/Main/Home/Controller/SHHomeViewController.m
+++ SmartHotel/SmartHotel/Classes/Main/Home/Controller/SHHomeViewController.m
@@ -10,13 +10,72 @@
 
 @interface SHHomeViewController ()
 
+/// 语言按钮
+@property (weak, nonatomic) IBOutlet UIButton *languageButton;
+
+/// 当前最后选中的语言名称
+@property (copy, nonatomic) NSString *selectLanguageName;
+
+
 @end
 
 @implementation SHHomeViewController
 
+/// 保存语言设置
+- (void)saveLanguageSetting:(NSString *)languageName {
+    
+    if (languageName.length <= 0 || [self.selectLanguageName isEqualToString:languageName]) {
+        return;
+    }
+    
+    self.selectLanguageName = languageName;
+    
+    [self.languageButton setTitle:self.selectLanguageName forState:UIControlStateNormal];
+    
+    [[NSUserDefaults standardUserDefaults] setObject:self.selectLanguageName forKey:@"LAGUAGEZ_NAME"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    [[SHLanguageTools shareSHLanguageTools] setLanguage];
+    
+    [SVProgressHUD showSuccessWithStatus:[[SHLanguageTools shareSHLanguageTools] getTextFromPlist:@"MAINVIEW" withSubTitle:@"Saved Successed!You should to restart app"]];
+}
+
+
+/// 设置语言
+- (IBAction)setLanguage {
+    
+    TYCustomAlertView *alertView = [TYCustomAlertView alertViewWithTitle:nil message:nil isCustom:YES];
+    
+    [alertView addAction:[TYAlertAction actionWithTitle:@"中文" style:TYAlertActionStyleDefault handler:^(TYAlertAction *action) {
+        
+        [self saveLanguageSetting:@"中文"];
+        
+    }]];
+    
+    [alertView addAction:[TYAlertAction actionWithTitle:@"English" style:TYAlertActionStyleDefault handler:^(TYAlertAction *action) {
+        
+        [self saveLanguageSetting:@"English"];
+        
+    }]];
+    
+    TYAlertController *alertController = [TYAlertController alertControllerWithAlertView:alertView preferredStyle:TYAlertControllerStyleAlert transitionAnimation:TYAlertTransitionAnimationScaleFade];
+    
+    alertController.backgoundTapDismissEnable = YES;
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.navigationItem.title = [NSString stringWithFormat:@"%@ %zd", [[SHLanguageTools shareSHLanguageTools] getTextFromPlist:@"MAINVIEW" withSubTitle:@"Room NO"], self.roomInfo.roomNumberDisplay];
+    
+    
+    // 当前选择的语言
+    self.selectLanguageName = [[NSUserDefaults standardUserDefaults] objectForKey:@"LAGUAGEZ_NAME"];
+    
+    [self.languageButton setTitle:self.selectLanguageName forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
